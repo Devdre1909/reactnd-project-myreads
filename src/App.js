a116179d28a75b18d41636f72c6a1d538fcfc746
Oluwadamilare <adegoketemitope1909@gmail.com>
2020-01-08 17:56:29 +0100

    :sparkles: Changing bookshelf made easy
    * BookShelf.js wont be needed again as Shelf component has taken over
    
    * Users can get real-time movement of book from one shelf to another

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css';
 import SearchBooks from './SearchBooks';
-import BookShelfs from './BookShelfs';
+import Shelf from './Shelf';
+import {Route, Link} from 'react-router-dom';
+
 
 
 class BooksApp extends Component {
   state = {
     books: [],
     loading: true,
-    showSearchPage: false
   }
 
   componentDidMount(){
@@ -22,29 +23,51 @@ class BooksApp extends Component {
       })
   }
 
+
+  changeShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+      .then((books) => {
+        this.componentDidMount()
+    })
+  }
  
   
   render() {  
 
     const {books, loading} = this.state;
 
-    const booksReading = books === [] ? books : books.filter(book => (book.shelf === "currentlyReading"));
+    const currentlyReading = books === [] ? books : books.filter(book => (book.shelf === "currentlyReading"));
     const wantToRead = books === [] ? books : books.filter(book => (book.shelf === "wantToRead"));
     const read = books === [] ? books : books.filter(book => (book.shelf === "read"));
 
     
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks />
-        ) : (
-          <BookShelfs 
-            CurrentlyReadingBook={booksReading}
-            WantToRead={wantToRead}
-            Read={read}
-            loading={loading} />
-        )
-        }
+        <Route
+          excact
+          path="/"
+          render={() => (
+            <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                {(loading) ? (<div style={{ margin: 'auto', fontWeight: 'bolder', textAlign: 'center' }}>Loading shelf...</div>) : (<Shelf onChangeShelf={(book, shelf) => {this.changeShelf(book, shelf)}} shelfTitle="Currently Reading" books={currentlyReading} />)}
+                {(loading) ? (<div style={{ margin: 'auto', fontWeight: 'bolder', textAlign: 'center'  }}>Loading shelf...</div>) : (<Shelf onChangeShelf={(book, shelf) => {this.changeShelf(book, shelf)}} shelfTitle="Want To Read" books={wantToRead} />)}
+                {(loading) ? (<div style={{ margin: 'auto', fontWeight: 'bolder', textAlign: 'center'  }}>Loading shelf...</div>) : (<Shelf onChangeShelf={(book, shelf) => {this.changeShelf(book, shelf)}} shelfTitle="Read" books={read} />)}
+                </div>
+              </div>
+            </div>
+          )}
+        />
+        <Route
+          path="/search"
+          render={() => (<SearchBooks />)}
+        />
+        <div className="open-search">
+          <Link to="/search" className="button">Add a book</Link>
+        </div>
     </div>
     )
   }
