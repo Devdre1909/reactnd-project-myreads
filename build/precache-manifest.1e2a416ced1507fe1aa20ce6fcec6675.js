b937fa4aeb608e8ece20cb675398440addd070d0
Oluwadamilare <adegoketemitope1909@gmail.com>
2020-01-09 16:02:16 +0100

    :construction_worker: Result of 'yarn build'

---
+++ build/precache-manifest.1e2a416ced1507fe1aa20ce6fcec6675.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4da0b2b7e5a521f11889",
+    "url": "/static/css/main.5bd4bab5.chunk.css"
+  },
+  {
+    "revision": "4da0b2b7e5a521f11889",
+    "url": "/static/js/main.4da0b2b7.chunk.js"
+  },
+  {
+    "revision": "088d886df3d7a4edd4ed",
+    "url": "/static/js/1.088d886d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "0685974abea53c7b4ca76c96179d8d12",
+    "url": "/index.html"
+  }
+];
